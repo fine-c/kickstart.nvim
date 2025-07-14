d8baf6654691438b44499a7e9734772be832cb93
Cyan <2802660864@qq.com>
2025-07-14 19:27:00 +0800

    关闭nvim-treesitter的语言解析器自动安装

--- init.lua
+++ init.lua
@@ -946,7 +946,7 @@ require('lazy').setup({
     opts = {
       ensure_installed = { 'diff', 'lua', 'luadoc', 'query' },
       -- Autoinstall languages that are not installed
-      auto_install = true,
+      auto_install = false,
       highlight = {
         enable = true,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
